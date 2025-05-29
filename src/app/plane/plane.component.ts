42cd4ebb9720dd1066f58b38dcb2c6dbf9f8c560
Roling551 <Roling95@gmail.com>
2025-05-29 20:11:24 +0200

    Add infinite background

--- src/app/plane/plane.component.ts
+++ src/app/plane/plane.component.ts
@@ -1,5 +1,5 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
-import panzoom from 'panzoom';
+import { AfterViewInit, Component, OnInit, signal } from '@angular/core';
+import panzoom, { PanZoom, Transform } from 'panzoom';
 import { DeepMap } from '../../shared/deepMap';
 
 @Component({
@@ -9,7 +9,6 @@ import { DeepMap } from '../../shared/deepMap';
   styleUrl: './plane.component.scss'
 })
 export class PlaneComponent implements OnInit, AfterViewInit {
-  
   items = new DeepMap<{x:number, y:number}, string>("x", "y")
 
   distance = 0.5
@@ -17,8 +16,13 @@ export class PlaneComponent implements OnInit, AfterViewInit {
   elementWidth = this.getX(1,0)+'px'
   elementHeight = this.getY(1,0)+'px'
 
+  transform = signal<Transform>({x:0, y:0, scale:1})
+
   public proportionX = 128
   public proportionY = 92
+
+  public positionX = 0
+  public positionY = 0
 
   ngOnInit(): void {
     this.items.set({x:1, y:1}, "t1")
@@ -41,9 +45,17 @@ export class PlaneComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit() {
+    const component = this
     const element = document.getElementById('zoom-target');
-    console.log(element)
-    panzoom(element!, {zoomDoubleClickSpeed: 1});
+    const textureSizeX = this.proportionX
+    const textureSizeY = this.proportionY
+    const instance = panzoom(element!, {zoomDoubleClickSpeed: 1});
+     instance.on('transform', function(e: any) {
+      const transform = e.getTransform()
+      component.transform.set(e.getTransform())
+      component.positionX = Math.floor(transform.x/textureSizeX/transform.scale)
+      component.positionY = Math.floor(transform.y/textureSizeY/transform.scale)
+    });
   }
 
   public getX(x: number, y:number) {
@@ -56,5 +68,10 @@ export class PlaneComponent implements OnInit, AfterViewInit {
 
   public onItemClick(item: string) {
     console.log(item)
+  }
+
+  get backgroundTransform(): string {
+    const { x, y, scale } = this.transform();
+     return `translate(${(-this.positionX-1) *this.proportionX }px, ${(-this.positionY-1) *this.proportionY }px)`;
   }
 }
